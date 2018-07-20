532fa2e1c276742df92715573f50f646cb4da075
Henry Zhang <henryzhang@SSCSs-MacBook-Pro-6.local>
2018-07-20 13:12:26 -0500

    I forgot how to use git

---
+++ HZ_presentation/LyX & PPT/auto/hps_slides_20180510_model.el
@@ -0,0 +1,28 @@
+(TeX-add-style-hook "hps_slides_20180510_model"
+ (lambda ()
+    (TeX-add-symbols
+     "be"
+     "ee")
+    (TeX-run-style-hooks
+     "ragged2e"
+     "array"
+     "colortbl"
+     "xcolor"
+     "gensymb"
+     "subfigure"
+     "textcomp"
+     "adjustbox"
+     "calc"
+     "booktabs"
+     "amsmath"
+     "graphicx"
+     "inputenc"
+     "latin1"
+     "babel"
+     "english"
+     "latex2e"
+     "beamer10"
+     "beamer"
+     "compress"
+     "t")))
+
---
+++ HZ_presentation/LyX & PPT/auto/hps_slides_berkeley_20170626.el
@@ -0,0 +1,25 @@
+(TeX-add-style-hook "hps_slides_berkeley_20170626"
+ (lambda ()
+    (TeX-add-symbols
+     "be"
+     "ee")
+    (TeX-run-style-hooks
+     "colortbl"
+     "xcolor"
+     "gensymb"
+     "subfigure"
+     "textcomp"
+     "adjustbox"
+     "booktabs"
+     "amsmath"
+     "graphicx"
+     "inputenc"
+     "latin1"
+     "babel"
+     "english"
+     "latex2e"
+     "beamer10"
+     "beamer"
+     "compress"
+     "t")))
+
